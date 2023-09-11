b4e122ae2fded8550c78877487a2a645227a545c
Albertyhu <hualbert@rocketmail.com>
2023-09-11 01:35:22 -0700

    added fun fact section

--- src/components/Home/funfact_section/counter.tsx
+++ src/components/Home/funfact_section/counter.tsx
@@ -1,13 +1,16 @@
 import CountUp from 'react-countup'
+
 type CounterType = {
 	number: number
 	delay?: number
 }
 
 export default function CounterComponent(props: CounterType) {
-	return (
+	return props.number ? 
 		<div className="count-outer relative block text-4xl text-[70px] leading-none text-white">
 			<CountUp end={props.number} />
 		</div>
-	)
+		:
+		<div>
+		</div>
 }
