6b5c80ce58869a26c208e08f1b5b0df8304a1544
Albertyhu <hualbert@rocketmail.com>
2023-12-08 23:38:12 -0800

    saving latest changes

--- src/customization/navigation-map.js
+++ src/customization/navigation-map.js
@@ -29,7 +29,7 @@ export const menuMap = [
 				subDirectory: []
 			},
 			{
-				url: '/services/artifical_turf_installation/',
+				url: '/services/artificial_turf_installation/',
 				label: 'Artifical Turf',
 				subDirectory: []
 			}
